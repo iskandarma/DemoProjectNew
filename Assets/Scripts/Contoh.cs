56865bbc1e6656d4c8507b01c35fa853f332eede
iskandarma <36181662+iskandarma@users.noreply.github.com>
2025-11-10 15:54:56 +0700

    add swipe script

--- Assets/Scripts/Contoh.cs
+++ Assets/Scripts/Contoh.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class Contoh : MonoBehaviour
 {
+    int Number1 = 10;
+    int Number2 = 7;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Test Log");
+        
+        int Temp;
+
+        Temp = Number1;
+        Number1 = Number2;
+        Number2 = Temp;
+
+        Debug.Log("Number1 = "+Number1);
+        Debug.Log("Number2 = "+Number2);
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Swipe.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Swipe : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        bool Result = ((1287 * 14 + 589) > (63 + 55 * 5)) || (21 < 10);
+
+        Debug.Log("Result = " + Result);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
