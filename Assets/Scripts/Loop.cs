50588d309c2e6daf25eec7f52861400d10f84729
iskandarma <36181662+iskandarma@users.noreply.github.com>
2025-11-11 09:19:35 +0700

    For loop

--- Assets/Scripts/Loop.cs
+++ Assets/Scripts/Loop.cs
@@ -5,14 +5,21 @@ public class Loope : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        int nyawa = 10;
+        int nyawa = 3;
 
+        // Pengulangan While
         while (nyawa > 0)
         {
             nyawa--;
             Debug.Log(nyawa);
         }
         Debug.Log("Game Over");
+
+        // Pengulangan For
+        for (int i = 0; i < 3; i++)
+        {
+            Debug.Log("Pengulangan ke-" + i);
+        }
     }
 
     // Update is called once per frame
